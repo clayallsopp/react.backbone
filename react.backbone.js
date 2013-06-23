c15403e1bd66d7ad53931166dad472164ed0df17
Clay Allsopp <clay.allsopp@gmail.com>
2013-06-22 19:13:57 -0600

    Create react.backbone.js

---
+++ react.backbone.js
@@ -0,0 +1,35 @@
+React.BackboneMixin = {
+    componentDidMount: function(domNode) {
+      // Whenever there may be a change in the Backbone data, trigger a reconcile.
+      var model = this.getModel();
+      // Detect if it's a collection
+      if (model instanceof Backbone.Collection) {
+          model.on('add remove reset', this.forceUpdate, this);
+      }
+      else if (model) {
+          var changeOptions = this.changeOptions || "change";
+          model.on(changeOptions, (this.onModelChange || this.forceUpdate), this);
+      }
+  },
+  componentWillUnmount: function() {
+      // Ensure that we clean up any dangling references when the component is destroyed.
+      this.getModel() && this.getModel().off(null, null, this);
+  }
+};
+
+React.createBackboneClass = function(spec) {
+    var currentMixins = spec.mixins || [];
+    _.extend(spec, {
+        mixins: currentMixins.concat([React.BackboneMixin]),
+        getModel: function() {
+            return this.props.model;
+        },
+        model: function() {
+            return this.getModel();
+        },
+        el: function() {
+            return this._rootNode && this.getDOMNode();
+        }
+    });
+    return React.createClass(spec);
+};
